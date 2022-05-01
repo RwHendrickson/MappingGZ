cc5f201d97e25471e615c7045d83d22a3b24cb5d
Bertrand Kotewall <bkotewall@gmail.com>
2022-05-01 16:56:41 -0500

    install ubuntu specific libs for geodjango runtime/devel

---
+++ install_gislibs_deb.sh
@@ -0,0 +1,7 @@
+sudo apt-get install binutils libproj-dev gdal-bin
+sudo apt-cache search geos
+sudo apt-get install libgeos-3.9.0 libgeos++-dev libgeos-dev libspatialite-dev libsqlite3-mod-spatialite  libspatialite7 
+sudo apt-get install geoip-bin  geoip-database  libgeoip-dev  libgeoip1  
+
+# not postgresql specific.  This is mostly for sqlite3.  We are installing these for completeness for django.
+sudo apt-get install libspatialite-dev  libspatialite7  libsqlite3-mod-spatialite librasterlite2-dev  librasterlite2-1  libsqlite3-mod-rasterlite2 libsqlite3-mod-virtualpg  libvirtualpg-dev  rasterlite2-bin  spatialite-bin libvirtualpg-dev   spatialite-gui 
